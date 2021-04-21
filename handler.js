76558a77b2664eaeee71b77595828680da80fc30
Vukkum Sai Prakash <vukkumsp@gmail.com>
2021-04-21 23:00:59 +0530

    fn to test returnzipfile added; fn to test nodejs & java added;

--- handler.js
+++ handler.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const fs = require("fs");
+const { exec } = require("child_process");
+
+const sampleZipPath = "./../sampleFiles/samplezip.zip";
+
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
@@ -15,17 +20,35 @@ module.exports.hello = async (event) => {
 };
 
 module.exports.returnzipfile = (event, context, callback) => {
-  let response =  {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+  let file = fs.readFile(sampleZipPath, "binary", (err, data)=>{
+    if(err) callback(err, null);
+    let response =  {
+      statusCode: 200,
+      headers:{
+        "Content-Type": "application/zip"
       },
-      null,
-      2
-    ),
-  };
+      body: data
+    };
+    callback(null, response);
+  });
+};
 
-  callback(null, response);
+module.exports.nodejsjavafn = (event, context, callback) => {
+  //This need to be tested in deployed state on AWS Lambda
+
+  exec("java -version",(error, stdout, stderr)=>{
+    if(error) callback(error, null);
+
+    let response = {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Node and Java both Runtimes available!',
+        }
+      ),
+    };
+    callback(null, response)
+  })
+
+
 };
